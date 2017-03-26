de0eaee6e1ee70bbb62d55e6c569b0d68b313d35
KAVYASEKAR <megnakavya1011@gmail.com>
2017-03-26 09:32:51 +0530

    Create Mapperclass.java

---
+++ Mapperclass.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.*; 
+public class Mapperclass extends Mapper<LongWritable, Text, Text, IntWritable> {
+	Text gen = new Text();
+	IntWritable o = new IntWritable(1);
+	public void map(LongWritable key, Text value, Context context) 
+			throws IOException, InterruptedException {
+		String[] a = value.toString().split(",");
+		int i = Integer.parseInt(a[1]);
+		if(i==1){
+			gen.set(a[4]);
+			context.write(gen, o);
+		}
+	}
